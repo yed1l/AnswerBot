21a49bfb4fd4f693f66efca13c1fb8ead17bad20
Zharkynuly Yedil <62800476+yed1l@users.noreply.github.com>
2020-10-05 13:17:32 +0600

    Initial Commit

---
+++ AnswerBot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnswerBot
+//
+//  Created by Yedil on 10/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
